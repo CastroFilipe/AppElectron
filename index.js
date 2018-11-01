49dde243472c55b0d8c451fce8e9abd92fd9b99b
Filipe Couto <Filipe.Couto.10@gmail.com>
2018-10-31 23:14:53 -0300

    criação da interface principal

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+//o index conterá o processo principal do electron.
+
 const electron = require('electron');
+
+/*Atribuição via desestruturação. 
+ * app é o processo principal da aplicação electron.
+ * BrowserWindow é necessário para a criação da janela de interação com o usuário.
+ * 
+ */
+const {app, BrowserWindow} = electron;
+
+
+app.on('ready', ()=>{
+    console.log('Electron iniciado e pronto(ready)');
+    new BrowserWindow({});//criará a janela de interação.
+});
