51d42ff5c6eae75912d47f9da64a67b2602e803c
Filipe Couto <Filipe.Couto.10@gmail.com>
2018-11-01 21:00:42 -0300

    Priemiro codigo HTML e modificacoes no index.js

--- index.js
+++ index.js
@@ -3,14 +3,19 @@
 const electron = require('electron');
 
 /*Atribuição via desestruturação. 
- * app é o processo principal da aplicação electron.
+ * app é o processo principal da aplicação electron. usado para Controlar os eventos do 
+ * ciclo de vida da sua aplicação.
  * BrowserWindow é necessário para a criação da janela de interação com o usuário.
  * 
  */
 const {app, BrowserWindow} = electron;
 
 
+//Evento do tipo ready emitido quando Electron tiver concluído a inicialização.
 app.on('ready', ()=>{
     console.log('Electron iniciado e pronto(ready)');
-    new BrowserWindow({});//criará a janela de interação.
+    const janela = new BrowserWindow({});//criará a janela de interação.
+    janela.loadURL(`file://${__dirname}/index.html`);//carregará o arquivo html na janela criada
+    //janela.loadURL("http://www.google.com"); //um exemplo maluco
 });
+
